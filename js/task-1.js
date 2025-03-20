a4b339249e52f9605d37b452f43526ddf99f9e5e
AlexLeonov2020 <alex.leon2010@icloud.com>
2025-03-20 08:07:55 +0200

    task over

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,14 @@
-function purchaseDroids(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  }
-
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if(customerCredits >= totalPrice){
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    } else {
+        return "Insufficient funds!";
+    }
 }
 
-// Перевірка функції
-console.log(purchaseDroids(5, 300, 2000)); // "You ordered 5 droids worth 1500 credits!"
-console.log(purchaseDroids(3, 500, 1000)); // "Insufficient funds!"
-console.log(purchaseDroids(2, 400, 1000)); // "You ordered 2 droids worth 800 credits!"
-console.log(purchaseDroids(0, 400, 1000)); // "You ordered 0 droids worth 0 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
